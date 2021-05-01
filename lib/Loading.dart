5f8166b8a285944ac953e9cf5942cada79a212eb
FatmaGuidara <fatma.guidara@insat.u-carthage.tn>
2021-05-01 14:44:08 +0100

    second Commit

--- lib/Loading.dart
+++ lib/Loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
